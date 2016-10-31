e8a467f1366bf8626f8818ec5fc2e7dfcbe63c17
Gokmen <gokmen_gs@live.nl>
2016-10-31 13:15:12 +0100

    Documenten toevoegen

---
+++ NSapi.py
@@ -0,0 +1,20 @@
+import requests
+import xmltodict
+username = 'gokmen.simsek@student.hu.nl'
+password = 'TQxDmfd3Ghmj9HiN5MFtnfAapCWj7bcx-0Yx3SJbtgS8r0bDGcpWcw'
+invoer = input('voer een station in')
+auth_details = (username, password)
+api_url = 'http://webservices.ns.nl/ns-api-avt?station={}'.format(invoer)
+response = requests.get(api_url, auth=auth_details)
+
+with open('vertrektijden.xml', 'w') as myXMLFile:
+ myXMLFile.write(response.text)
+
+
+vertrekXML = xmltodict.parse(response.text)
+print('Dit zijn de vertrekkende treinen:')
+for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+ eindbestemming = vertrek['EindBestemming']
+ vertrektijd = vertrek['VertrekTijd'] # 2016-09-27T18:36:00+0200
+ vertrektijd = vertrektijd[11:16] # 18:36
+ print('Om {} vertrekt een trein naar {}'.format(vertrektijd, eindbestemming))
