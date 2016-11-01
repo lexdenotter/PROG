36396f70c3d28cdd9299e0e1867b590ada2c0aae
Gokmen <gokmen_gs@live.nl>
2016-11-01 12:52:57 +0100

    API functie aangepast

--- NSapi.py
+++ NSapi.py
@@ -1,36 +1,32 @@
 import requests
 import xmltodict
-import datetime
 
-username = 'gokmen.simsek@student.hu.nl'
-password = 'TQxDmfd3Ghmj9HiN5MFtnfAapCWj7bcx-0Yx3SJbtgS8r0bDGcpWcw'
-invoer = input('voer een station in')
-auth_details = (username, password)
-api_url = 'http://webservices.ns.nl/ns-api-avt?station={}'.format(invoer)
-response = requests.get(api_url, auth=auth_details)
+station = 'ut'
+def reisinformatie(station):
+    #api aanroepen
+    username = 'gokmen.simsek@student.hu.nl'
+    password = 'TQxDmfd3Ghmj9HiN5MFtnfAapCWj7bcx-0Yx3SJbtgS8r0bDGcpWcw'
+    auth_details = (username, password)
+    api_url = 'http://webservices.ns.nl/ns-api-avt?station={}'.format(station)
+    response = requests.get(api_url, auth=auth_details)
 
-formatted_time = datetime.datetime.now().strftime('%I') # I
-with open('vertrektijden.xml', 'w') as myXMLFile:
- myXMLFile.write(response.text)
+    with open('vertrektijden.xml', 'w') as myXMLFile:
+        myXMLFile.write(response.text)
+        vertrekXML = xmltodict.parse(response.text)
+        vertrekXML = vertrekXML['ActueleVertrekTijden']
+        elementen = vertrekXML['VertrekkendeTrein']
+        for vertrek in elementen:
+            vertrektijd = vertrek['VertrekTijd']
+            vertrektijd = vertrektijd[11:16]
+            eindbestemming = vertrek['EindBestemming']
+            try:
+                reistip = vertrek['ReisTip']
+            except:
+                reistip = None
 
-
-vertrekXML = xmltodict.parse(response.text)
-print('Dit zijn de vertrekkende treinen:')
-for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
- eindbestemming = vertrek['EindBestemming']
- vertrektijd = vertrek['VertrekTijd'] # 2016-09-27T18:36:00+0200
- vertrektijd = vertrektijd[11:16] # 18:16
- print('Om {} vertrekt een trein naar {}'.format(vertrektijd, eindbestemming))
-
-with open('vertrektijden.xml') as stations:
-  filecontent = stations.read()
-  doc = xmltodict.parse(filecontent)
-  doc = doc['ActueleVertrekTijden']
-  station_elementen = doc['VertrekkendeTrein']
-  for element in station_elementen:
-    try:
-        print('Treinsoort {} en reistip {}'.format(element['TreinSoort'], element['ReisTip']))
-    except:
-        pass
-
+            if reistip == None:
+                print("{} en {}".format(vertrektijd, eindbestemming))
+            else:
+                print("{} en {} en {}".format(vertrektijd, eindbestemming, reistip))
+reisinformatie(station)
 
