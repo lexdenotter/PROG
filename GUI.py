19d1035c15ac9aa44f24bd13c851a3f81a6d550b
lexdenotter <lexdenotter@home.nl>
2016-11-02 10:42:37 +0100

    laatste fix

--- GUI.py
+++ GUI.py
@@ -1,8 +1,8 @@
 from tkinter import *
-
 from GUI_Utrecht import toonVenster1
 from GUI_Ander import toonVenster2
 
+#gegevens voor main scherm
 root = Tk()
 root.title('Reizigersinformatie')
 root.geometry('790x596')
@@ -12,10 +12,11 @@ x = Label (image = bg_image)
 x.grid(row = 0, column = 0)
 
 
-# Buttons
+#button1(huidig station)
 button1 = Button(master=root, text='Reisinformatie Huidig Station', command=toonVenster1, background='#01236a', foreground='white', font=('Verdana', 13))
 button1.place(x=110, y=400)
 
+#button2(ander station)
 button2 = Button(master=root, text='Reisinformatie Ander Station', command=toonVenster2, background='#01236a', foreground='white', font=('Verdana', 13))
 button2.place(x=400, y=400)
 
--- GUI_Ander.py
+++ GUI_Ander.py
@@ -1,5 +1,7 @@
 from tkinter import *
+from NSapi import *
 
+#functie om scherm aan te maken
 def toonVenster2():
     root = Tk()
     root.geometry('790x596')
