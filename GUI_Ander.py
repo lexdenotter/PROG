3cff31b2f30f0faf70d5578175d31ffda8a72110
Paul Verhoeven <pjmverhoeven@home.nl>
2016-11-02 13:26:54 +0100

    dikke fixxxx

--- GUI_Ander.py
+++ GUI_Ander.py
@@ -1,8 +1,32 @@
 from tkinter import *
 
+from NSapi import *
+
 def toonVenster2():
     root = Tk()
     root.geometry('790x596')
     root.resizable(width=False, height=False)
-    root.title('Reisinformatie Ander Station')
+    root.title('Reisinformatie Huidig Station')
     root.configure(background='#ffcf1a')
+
+    button1 = Button(master=root, text='Haal Vertrektijden op', command=aanroeputrecht, background='#01236a', foreground='white', font=('Verdana', 13))
+    button1.place(x=10, y=10)
+    button1.pack()
+
+    button2 = Button(master=root, text='Haal Vertrektijden op van Tilburg', command=aanroeptilburg, background='#01236a', foreground='white', font=('Verdana', 13))
+    button2.place(x=10, y=10)
+    button2.pack()
+
+    button3 = Button(master=root, text='Haal Vertrektijden op van Breda', command=aanroepbreda, background='#01236a', foreground='white', font=('Verdana', 13))
+    button3.place(x=10, y=10)
+    button3.pack()
+
+
+def aanroeputrecht():
+    return reisinformatie('ut')
+
+def aanroeptilburg():
+    return reisinformatie('tilburg')
+
+def aanroepbreda():
+    return reisinformatie('breda')
